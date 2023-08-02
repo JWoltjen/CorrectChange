5b574be1b1acf5ffc71d2dfc9ec41a2a6e8562b8
Jeff Woltjen (he/him) <Jeff.Woltjen@gmail.com>
2023-08-02 14:06:42 -0500

    Add project files.

---
+++ CorrectChange/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Diagnostics;
+using System.Text;
+
+namespace CorrectChange
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.ReadLine();
+        }
+
+    }
+}
